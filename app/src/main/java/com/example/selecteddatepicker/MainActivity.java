172f8414ba133f3aba4233c070d45819de2ced6f
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-01 15:05:05 +0630

    insert date picker , show selected date in textview

--- app/src/main/java/com/example/selecteddatepicker/MainActivity.java
+++ app/src/main/java/com/example/selecteddatepicker/MainActivity.java
@@ -1,7 +1,17 @@
 package com.example.selecteddatepicker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText txtDate = (EditText) findViewById(R.id.txt_date);
+        Button btnShowDate = (Button) findViewById(R.id.btn_showDate);
+        final TextView txtSelectedDate = (TextView) findViewById(R.id.txt_selectedDate);
+        txtDate.setInputType(InputType.TYPE_NULL);
+        txtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cldr = Calendar.getInstance();
+                int year = cldr.get(Calendar.YEAR);
+                int month = cldr.get(Calendar.MONTH);
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog picker = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        txtDate.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                    }
+                },year,month,day);
+                picker.show();
+            }
+        });
+
+        btnShowDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtSelectedDate.setText("Selected date is " + txtDate.getText().toString());
+            }
+        });
     }
 }
